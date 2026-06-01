90bacf1852777ce873a65829efa9a9d8e43fd71f
Erick Palomino <u20231a500@upc.edu.pe>
2026-06-01 12:01:14 -0500

    first commit

---
+++ src/main/java/com/uniride/uniridefinanceservice/UnirideFinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uniride.uniridefinanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnirideFinanceServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UnirideFinanceServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/uniride/uniridefinanceservice/UnirideFinanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniride.uniridefinanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnirideFinanceServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
